94b1008fecc833953092fb2592e19073c7da8cbe
Mirzakhanov Rauf <79833498058@yandex.ru>
2023-03-10 20:33:24 +0700

    Домашняя работа

--- Program.cs
+++ Program.cs
@@ -10,6 +10,10 @@ namespace Project
     {
         static void Main(string[] args)
         {
+            Console.Write("Введите ваше имя: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Привет, " + name + "!");
+
         }
     }
 }

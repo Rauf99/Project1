823273a14eb2c0c43044508b45636a4efbb60cf3
Mirzakhanov Rauf <79833498058@yandex.ru>
2023-03-19 22:10:12 +0700

    Домашняя работа 2

--- Program.cs
+++ Program.cs
@@ -6,15 +6,62 @@ using System.Threading.Tasks;
 
 namespace Project
 {
-   /* internal class Program
+    public class Homework2
     {
-        static void Main(string[] args)
+        public static void Main()
         {
-            Console.Write("Введите ваше имя: ");
-            string name = Console.ReadLine();
-            Console.WriteLine("Привет, " + name + "!");
+            Console.Write("Введите размер массива:\t ");
+            int elementsCount = 0;
+            try
+            {
+                elementsCount = UInt16.Parse(Console.ReadLine());
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Ошибка! Вы вели некорректное значение. ");
+                return;
+            }
 
+            if (elementsCount < 2)
+            {
+                Console.WriteLine("Ошибка! Введите значение больше 1");
+                return;
+            }
+            else
+            {
+                int[] array = new int[elementsCount];
+                for (int i = 0; i < elementsCount; i++)
+                {
+                    Console.WriteLine($"Введите элемент массива под индексом {i}:\t");
+                    try
+                    {
+                        array[i] = int.Parse(Console.ReadLine());
+                    }
+                    catch (Exception)
+                    {
+                        Console.WriteLine($"Ошибка! Некорректное значение индекса #{i}");
+                        return;
+                    }
+                }
+                int[] intArray = new int[array.Length];
+                for (int i = 0; i < array.Length; i++)
+                {
+                    intArray[i] = Convert.ToInt32(array[i]);
+                    int[] intArrayCopy = intArray.Distinct().ToArray();
+                    Array.Sort(intArrayCopy);
+                    Array.Reverse(intArrayCopy);
+                    int checkArrayCopySize = intArrayCopy.Count();
+                    if (checkArrayCopySize > 1)
+                    {
+                        Console.Write($"Второе наибольшее число в вашем массиве: {intArrayCopy[1]}");
+                        Console.WriteLine();
+                    }
+                    else
+                    {
+                        Console.WriteLine("\n\t\aОшибка! Невозможно вывести второе наибольшее число. В данном массиве только одно наибольшее число.");
+                    }
+                }
+            }
         }
     }
-    */
 }
